d5bf658b8d4f56a87ce3b8c904adb0241a1a7bb5
James Ouk <ouk.j@northeastern.edu>
2022-01-21 20:25:52 -0500

    removed .gitignore for app/build/

---
+++ app/build/generated/source/buildConfig/debug/edu/neu/madcourse/numad22sp_jamesouk/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package edu.neu.madcourse.numad22sp_jamesouk;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "edu.neu.madcourse.numad22sp_jamesouk";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
