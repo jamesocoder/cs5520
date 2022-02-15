1b6b9e1b176dfb091f0fe413d46d923bce74a291
James Ouk <ouk.j@northeastern.edu>
2022-02-15 00:08:02 -0500

    a04adapter first draft

---
+++ app/src/main/java/edu/neu/madcourse/numad22sp_jamesouk/A04Adapter.java
@@ -0,0 +1,37 @@
+package edu.neu.madcourse.numad22sp_jamesouk;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+
+/*
+This defines a container class that holds an array of the A04Holder/A04Card objects that we defined
+ */
+public class A04Adapter extends RecyclerView.Adapter<A04Holder> {
+
+    private final ArrayList<A04Card> itemList;
+    private A04Card listener;
+
+    public A04Adapter(ArrayList<A04Card> itemList) { this.itemList = itemList; }
+
+    public void setOnItemClickListener(A04Card listener) { this.listener = listener; }
+
+    @Override
+    public A04Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_a04, parent, false);
+        return new A04Holder(view, listener);
+    }
+
+    @Override
+    public void onBindViewHolder(A04Holder holder, int position) {
+        A04Card currentItem = itemList.get(position);
+
+        holder.title.setText(currentItem.getTitle());
+        holder.url.setText(currentItem.getUrl());
+    }
+
+    @Override
+    public int getItemCount() { return itemList.size(); }
+}
