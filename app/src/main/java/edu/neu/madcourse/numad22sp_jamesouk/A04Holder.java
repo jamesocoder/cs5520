93708d9357b0e237dcf1dd637e6bdd95b03222b1
James Ouk <ouk.j@northeastern.edu>
2022-02-14 23:55:52 -0500

    a04holder first draft

--- app/src/main/java/edu/neu/madcourse/numad22sp_jamesouk/A04Holder.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_jamesouk/A04Holder.java
@@ -10,6 +10,20 @@ public class A04Holder extends RecyclerView.ViewHolder {
 
     public A04Holder(View itemView, final A04Card listener) {
         super(itemView);
-        title = itemView.findViewById(R.id)
+        title = itemView.findViewById(R.id.card_title);
+        url = itemView.findViewById(R.id.card_url);
+
+        // This registers a card's listener, causing it to perform the action defined in A04Card.java
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null) {
+                    int pos = getLayoutPosition();
+                    if (pos != RecyclerView.NO_POSITION) {
+                        listener.onItemClick(pos);
+                    }
+                }
+            }
+        });
     }
 }
