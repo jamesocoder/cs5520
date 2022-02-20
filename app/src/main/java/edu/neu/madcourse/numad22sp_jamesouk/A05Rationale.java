feb19cf1fceaf4ada74428a0f1df9d1ed26e9ff7
James Ouk <ouk.j@northeastern.edu>
2022-02-20 01:18:05 -0500

    A05 act_rationale done

---
+++ app/src/main/java/edu/neu/madcourse/numad22sp_jamesouk/A05Rationale.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.numad22sp_jamesouk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class A05Rationale extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a05_activity_rationale);
+    }
+}
